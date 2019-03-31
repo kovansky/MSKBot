caf39cd23183555ee14a67d4695ab3afe5efcfa9
Kovansky <root@f4dev.me>
2019-04-01 00:18:34 +0200

    :gemini: Create createRelatedChannels util.

---
+++ utils/createRelatedChannels.js
@@ -0,0 +1,27 @@
+exports.run = (guild, role) => {
+    let names = {
+        category: role.name,
+        channel:  `ogólny-${role.name.toLowerCase()}`
+    };
+
+    if(!guild.channels.find(chan => chan.name === names.category && chan.type === "category")) {
+        guild.createChannel(names.category, "category", [
+            {
+                id:   guild.defaultRole.id,
+                deny: ["VIEW_CHANNEL"]
+            },
+            {
+                id:    role.id,
+                allow: ["VIEW_CHANNEL"]
+            }
+        ])
+             .then((category) => {
+                 guild.createChannel(names.channel)
+                      .then((channel) => {
+                          channel.setParent(category);
+                      })
+                      .catch(console.error);
+             })
+             .catch(console.error);
+    }
+};
