0ae948e00f5a6249b2b8816c5c48b7f0e7afb0dc
Kovansky <root@f4dev.me>
2019-03-31 19:59:03 +0200

    :calendar: Register events.

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-const Discord = require('discord.js'),
-      client = Discord.Client();
+global.appRoot = path.resolve(__dirname);
+
+const Discord = require("discord.js"),
+      client  = Discord.Client(),
+      fs      = require("fs"),
+
+      config  = require(global.appRoot + "/config.json");
+
+// Register events
+fs.readdir("./events/", (err, files) => {
+    if(err) {
+        console.error(err);
+    }
+
+    files.forEach(file => {
+        let eventFunction = require(`./events/${file}`),
+            eventName     = file.split(".")[0];
+
+        client.on(eventName, (...args) => eventFunction.run(client, ...args));
+    });
+});
