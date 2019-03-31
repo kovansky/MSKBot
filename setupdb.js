eb2aae8f81154ee53200ef59fef9662651da3973
Kovansky <root@f4dev.me>
2019-03-31 23:57:41 +0200

    :hammer: Improved creating sql database - message not shown always.

--- setupdb.js
+++ setupdb.js
@@ -1,8 +1,11 @@
 exports.run = (sql) => {
     sql.serialize(() => {
-        sql.run("create table if not exists cities (id INTEGER not null constraint cities_pk primary key" +
-                    " autoincrement, name TEXT not null, role INTEGER not null)");
-
-        console.log("Databases created");
-    })
-}
+        sql
+            .run("create table if not exists cities (id INTEGER not null constraint cities_pk primary key" +
+                     " autoincrement, name TEXT not null, role INTEGER not null)", () => {
+                if(this.changes > 0) {
+                    console.log("Databases created");
+                }
+            });
+    });
+};
