f98b2f0103710fc1d0308eb4a706d0d6cad2db32
Kovansky <root@f4dev.me>
2019-04-01 21:48:58 +0200

    :pencil2: Register command.

--- modules/miasto/module.js
+++ modules/miasto/module.js
@@ -8,98 +8,102 @@ exports.run = (sql, client, message, [command, ...args]) => {
         global.guild = message.guild;
     }
 
-    switch(command) {
-        case "dodaj":
-            city = args.join(" ");
+    if(command !== undefined) {
+        switch(command) {
+            case "dodaj":
+                city = args.join(" ");
 
-            sql.serialize(() => {
-                sql.get("select id from cities where name = ?", [city], (err, row) => {
-                    if(err) {
-                        console.error(err);
+                sql.serialize(() => {
+                    sql.get("select id from cities where name = ?", [city], (err, row) => {
+                        if(err) {
+                            console.error(err);
+                        }
+
+                        if(row === undefined && global.guild !== null && global.guild !== undefined && !global.guild.roles.find(role => role.name === city)) {
+                            let data = {
+                                name:        city,
+                                color:       config.cities.defaultColor,
+                                mentionable: true
+                            };
+
+                            global.guild.createRole(data)
+                                  .then((role) => {
+                                      sql.run("insert into cities (name, role) VALUES (?, ?)", [
+                                          role.name, role.id
+                                      ], (err) => {
+                                          if(err) {
+                                              console.error(err);
+                                          }
+
+                                          createRelatedChannels.run(client, guild, role);
+
+                                          message.reply(`utworzono ${role.name}.\nJeśli chcesz dołączyć, użyj \`${config.prefix}miasto ${role.name}\``);
+                                      });
+                                  })
+                                  .catch(console.error);
+                        } else {
+                            message.reply(`to miasto już istnieje`);
+                        }
+                    });
+                });
+                break;
+            default:
+                sql.serialize(() => {
+                    city = command;
+
+                    if(args.length > 0) {
+                        city += " " + args.join(" ");
                     }
 
-                    if(row === undefined) {
-                        let data = {
-                            name:        city,
-                            color:       config.cities.defaultColor,
-                            mentionable: true
-                        };
+                    sql.get("select id, name, role from cities where name = ?", [city], (err, row) => {
+                        if(err) {
+                            console.error(err);
+                        }
 
-                        global.guild.createRole(data)
-                              .then((role) => {
-                                  sql.run("insert into cities (name, role) VALUES (?, ?)", [
-                                      role.name, role.id
-                                  ], (err) => {
-                                      if(err) {
-                                          console.error(err);
+                        let role;
+                        global.guild.fetchMember(message.author)
+                              .then((member) => {
+                                  if(row !== undefined) {
+                                      role = global.guild.roles.get(row.role);
+
+                                      if(!member.roles.has(role.id)) {
+                                          member.addRole(role).catch(console.error);
+
+                                          message.reply(`dodano do ${city}`);
+
+                                          createRelatedChannels.run(client, global.guild, role);
+                                      } else {
+                                          message.reply(`już jesteś w ${city}`);
+                                      }
+                                  } else if(global.guild !== null && global.guild !== undefined && global.guild.roles.find(role => role.name === city)) {
+                                      role = global.guild.roles.find(role => role.name === city);
+
+                                      if(!member.roles.has(role.id)) {
+                                          member.addRole(role).catch(console.error);
+
+                                          message.reply(`dodano do ${city}`);
+
+                                          createRelatedChannels.run(client, global.guild, role);
+                                      } else {
+                                          message.reply(`już jesteś w ${city}`);
                                       }
 
-                                      createRelatedChannels.run(client, guild, role);
-
-                                      message.reply(`utworzono ${role.name}`);
-                                  });
+                                      sql.run("insert into cities (name, role) VALUES (?, ?)", [
+                                          city, role.id
+                                      ], (err) => {
+                                          if(err) {
+                                              console.error(err);
+                                          }
+                                      });
+                                  } else {
+                                      message.reply(`tego miasta jeszcze nie mamy.\nJeśli chcesz je dodać, wpisz \`${config.prefix}miasto dodaj ${city}\``);
+                                  }
                               })
                               .catch(console.error);
-                    }
+                    });
                 });
-            });
-            break;
-        default:
-            sql.serialize(() => {
-                city = command;
-
-                if(args.length > 0) {
-                    city += " " + args.join(" ");
-                }
-
-                console.log(city);
-
-                sql.get("select id, name, role from cities where name = ?", [city], (err, row) => {
-                    if(err) {
-                        console.error(err);
-                    }
-
-                    let role;
-                    global.guild.fetchMember(message.author)
-                          .then((member) => {
-                              if(row !== undefined) {
-                                  role = global.guild.roles.get(row.role);
-
-                                  if(!member.roles.has(role.id)) {
-                                      member.addRole(role).catch(console.error);
-
-                                      message.reply(`dodano do ${city}`);
-
-                                      createRelatedChannels.run(client, global.guild, role);
-                                  } else {
-                                      message.reply(`już jesteś w ${city}`);
-                                  }
-                              } else if(global.guild !== null && global.guild !== undefined && global.guild.roles.find(role => role.name === city)) {
-                                  role = global.guild.roles.find(role => role.name === city);
-
-                                  if(!member.roles.has(role.id)) {
-                                      member.addRole(role).catch(console.error);
-
-                                      message.reply(`dodano do ${city}`);
-
-                                      createRelatedChannels.run(client, global.guild, role);
-                                  } else {
-                                      message.reply(`już jesteś w ${city}`);
-                                  }
-
-                                  sql.run("insert into cities (name, role) VALUES (?, ?)", [
-                                      city, role.id
-                                  ], (err) => {
-                                      if(err) {
-                                          console.error(err);
-                                      }
-                                  });
-                              } else {
-                                  message.reply(`tego miasta jeszcze nie mamy.\nJeśli chcesz je dodać, wpisz \`${config.prefix}miasto dodaj ${city}\``);
-                              }
-                          })
-                          .catch(console.error);
-                });
-            });
+        }
+    } else {
+        message.reply(`użyj \`${config.prefix}miasto [nazwa miasta]\`, aby dołączyć do miasta, lub \`${config.prefix}miasto dodaj [nazwa miasta]\`, aby utworzyć nieistniejące miasto.`);
     }
 };
---
+++ modules/zarejestruj/module.js
@@ -0,0 +1,43 @@
+const config                = require(global.appRoot + "/config.json"),
+      createRelatedChannels = require(global.appRoot + "/utils/createRelatedChannels.js"),
+      cityModule            = require(global.appRoot + "/modules/miasto/module.js");
+
+exports.run = (sql, client, message, args) => {
+    if(message.guild !== null && message.guild !== undefined) {
+        global.guild = message.guild;
+    }
+
+    if(args.length >= 3) {
+        global.guild.fetchMember(message.author)
+              .then((member) => {
+                  let user     = {
+                          name:    args[0],
+                          surname: args[1],
+                          city:    args.slice(2).join(" ")
+                      },
+                      nickname = user.name + " " + user.surname + " (" + user.city + ")";
+
+                  if(nickname.length > 32) {
+                      nickname = user.name.charAt(0) + ". " + user.surname + " (" + user.city + ")";
+                  }
+
+                  console.log(user.city);
+
+                  member.setNickname(nickname)
+                        .then(() => {
+                            cityModule.run(sql, client, message, [user.city]);
+
+                            message.reply(`zarejestrowano jako \`${nickname}\`!`);
+                        })
+                        .catch((err) => {
+                            console.error(err);
+
+                            if(err.code === 50035 && err.message.indexOf("32 or fewer") !== -1) {
+                                message.reply("nick jest za długi!");
+                            }
+                        });
+              });
+    } else {
+        message.reply(`użyj \`${config.prefix}zarejestruj [imię] [nazwisko] [nazwa miasta]\`, aby się zarejestrować.`);
+    }
+};
